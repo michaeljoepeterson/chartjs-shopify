691766ac8f9f7d33d92d332e9d502157a9928996
Michael Peterson <michaeljoepeterson@gmail.com>
2020-09-11 20:17:17 -0600

    first skeletal stucture of chart class and start setup of chart app

--- js/chart-app.js
+++ js/chart-app.js
@@ -1,3 +1,57 @@
-class ChartApp(){
-    
+class ChartApp{
+    chartIds;
+    chartTitles;
+    chartBackgroundColors;
+
+    baseCharts = {};
+    //intake shopify created options and create charts
+    constructor(options){
+        try{
+            this.checkOptions();
+            this.chartIds = this.buildIds(options.chartTitles);
+            //will be dataset label
+            this.chartTitles = options.chartTitles;
+            this.chartBackgroundColors = options.chartBackgroundColors;
+            this.chartBorderColors = options.chartBackgroundColors;
+            //array of arrays
+            this.allChartData = options.allChartData;
+            console.log('build charts');
+        }
+        catch(err){
+            console.log('error with chart app: ',err);
+            throw err;
+        }
+    }
+
+    buildCharts(){
+
+    }
+
+    cleanData(){
+
+    }
+
+    buildIds(chartTitles){
+        let ids = chartTitles.map(title => {
+            let id = title.replace(/\s/g,'');
+            return id;
+        });
+
+        return ids;
+    }
+
+    checkOptions(options){
+        if(!options){
+            let err ={
+                message:'No chart data'
+            };
+            throw err;
+        }
+        if(!options.allChartData){
+            let err ={
+                message:'Missing chart data'
+            };
+            throw err;
+        }
+    }
 }
--- js/chart.js
+++ js/chart.js
@@ -1,3 +1,53 @@
 class BaseChart{
-    
+    //base options
+    //limit to single dataset per chart due to shopify limitations in block setting schema
+    chartOptions = {
+        type:null,
+        data:{
+            labels:[],
+            datasets:[]
+        },
+        options:{}
+    };
+
+    options = {};
+    colorKey = 'color';
+    chartId;
+    chart;
+
+    constructor(options){
+        if(options){
+            try{
+                this.chartOptions.type = options.type;
+                this.chartOptions.data.labels = options.labels;
+                this.chartOptions.data.datasets = this.buildDatasets(options.datasets);
+                this.chartId = options.chartId;
+                this.buildChart();
+            }
+            catch(err){
+                console.log('error building charts',err);
+            }
+        }
+    }
+
+    buildDatasets = (datasetOptions) =>{
+        let datasets = [{}];
+
+        for(let option in datasetOptions){
+            if(option.toLowerCase().includes(this.colorKey)){
+                if(datasetOptions[option].length === 1){
+                    datasets[0][option] = datasetOptions[option][0];
+                }
+            }
+            datasets[0][option] = datasetOptions[option];
+        }
+
+        return datasets;
+    }
+
+    buildChart(){
+        let ctx = document.getElementById(this.chartId).getContext('2d');
+        this.chart = new Chart(ctx, this.chartOptions);
+    }
+
 }
